8c8e430337d9cbdcc8b74613fd2d623a096cd6fa
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-14 23:19:04 -0300

    State com class component

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -3,16 +3,43 @@ import Botao from "../Botao";
 import styles from './Formulario.module.scss'
 
 class Formulario extends React.Component {
+    state = { 
+        nomeDaTarefa: "" ,
+        tempo: "00:00"
+    }
+
+    adicionarTarefa(evento: React.FormEvent<HTMLFormElement>) {
+        evento.preventDefault()
+        console.log('state: ', this.state)
+    }
+
     render(): React.ReactNode {
         return (
-            <form className={styles.novaTarefa}>
+            <form className={styles.novaTarefa} onSubmit={this.adicionarTarefa.bind(this)}>
                 <div className={styles.inputContainer}>
                     <label htmlFor="tarefa">Adicione um novo estudo</label>
-                    <input type="text" name="tarefa" id="tarefa" placeholder="O que você quer estudar" required/>
+                    <input value={this.state.nomeDaTarefa}
+                        onChange={evento => this.setState({...this.state, nomeDaTarefa: evento.target.value})} 
+                        type="text" 
+                        name="tarefa" 
+                        id="tarefa" 
+                        placeholder="O que você quer estudar" 
+                        required
+                    
+                    />
                 </div>
                 <div className={styles.inputContainer}>
                     <label htmlFor="tempo">Tempo</label>
-                    <input type="time" name="tempo" id="tempo" min="00:00:00" max="01:30:00" step="1" required/>
+                    <input value={this.state.tempo} 
+                        onChange={evento => this.setState({...this.state, tempo: evento.target.value})}
+                        type="time" 
+                        name="tempo" 
+                        id="tempo" 
+                        min="00:00:00" 
+                        max="01:30:00" 
+                        step="1" 
+                        required
+                    />
                 </div>
                 <Botao>Adicionar</Botao>
             </form>
